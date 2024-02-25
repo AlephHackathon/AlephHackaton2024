887e4f2681d3a5a3e2afa8cbd02b27ea16d09c16
Ashef <hugo1.fontaine@epitech.eu>
2024-02-25 12:07:03 +0100

    fix: work on mobile

--- AlephApp/App.js
+++ AlephApp/App.js
@@ -1,33 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import axios from 'axios'
-import { importAccountFromPrivateKey } from '@aleph-sdk/ethereum';
 import { TextInput, TouchableOpacity, ScrollView, StyleSheet, Text, View, Button } from 'react-native';
-// import { ThemeProvider, useTheme } from './ThemeContext';
-
-// function ThemeSwitch() {
-//   const { isDarkMode, toggleTheme } = useTheme();
-
-//   return (
-//     <View style={styles.themeSwitch}>
-//       <Text style={styles.text}>Thème {isDarkMode ? 'sombre' : 'clair'}</Text>
-//       <Button title="Changer de thème" onPress={toggleTheme} />
-//     </View>
-//   );
-// }
 
 export default function App() {
-  //const account = getAccountFromProvider(window.ethereum);
-//
-  //// initialize the wallet
-  //account.init();
-//
-  //// increase a flow (in ALEPH/hour)
-  //const receiver = "12399e820b4341c6d0d5bb916ad2fa2004fd51ee0a6dc4890fa62cd73dd505a1";
-  //account.increaseALEPHFlow(receiver, 1);
-//
-  //// get the net flow of ALEPH/hour
-  //const flow = account.getALEPHFlow();  var [text, setText] = useState('');
+  var [text, setText] = useState('');
   var [Status, setStatus] = useState('');
   var [item_hash, setitem_hash] = useState('');
   var [Reception, setReception] = useState('');
@@ -49,16 +26,11 @@ export default function App() {
       console.log(err);
     })
   };
-
-  // const { isDarkMode } = useTheme();
   
   return (
-    // <ThemeProvider>
     <ScrollView>
       <View style={styles.container}>
-      {/* <View style={[styles.container, isDarkMode ? styles.darkContainer : styles.lightContainer]}> */}
-        {/* <ThemeSwitch/> */}
-        <Text>Hello</Text>
+        <Text style={styles.text}>Hello</Text>
 
         <TextInput
           style={styles.input}
@@ -71,18 +43,15 @@ export default function App() {
           <Text style={styles.buttonText}>Process</Text>
         </TouchableOpacity>
 
-        <Text style={styles.text}>Status: {Status}</Text>
-        <Text style={styles.text}>item_hash: {item_hash}</Text>
-        <Text style={styles.text}>Reception time: {Reception}</Text>
-        <Text style={styles.text}>Sender: {Sender}</Text>
-        <Text style={styles.text}>Chain: {Chain}</Text>
+        <Text style={styles.text}>Status: <Text>{Status}</Text></Text>
+        <Text style={styles.text}>item_hash: <Text>{item_hash}</Text></Text>
+        <Text style={styles.text}>Reception time: <Text>{Reception}</Text></Text>
+        <Text style={styles.text}>Sender: <Text>{Sender}</Text></Text>
+        <Text style={styles.text}>Chain: <Text>{Chain}</Text></Text>
 
         <StatusBar style="auto"/>
-        {/* <StatusBar style={isDarkMode ? 'light' : 'dark'} /> */}
       </View>
-    /</ScrollView>
-    // </ThemeProvider>
-
+    </ScrollView>
   );
 }
 
@@ -129,3 +98,4 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
 });
+
